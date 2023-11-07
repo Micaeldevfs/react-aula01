412a05f81fb398af583a123bbba6ed90080643a8
Micael Duarte <119959532+Micaeldevfs@users.noreply.github.com>
2023-11-07 15:33:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import imgprog from "./assets/programar.svg"
+
+function App() {
+
+  return (
+    <>
+      <h1>Micael Duarte de Oliveira Neves</h1>
+      <h2>Idade: 29</h2>
+      <br />
+      <h3>Eu sou um entusiasta de programação em busca constante de aprendizado e crescimento no universo do desenvolvimento full stack. Atualmente, estou mergulhado no mundo da programação na Infinity School <img align="center" alt="infinityschool" width="25px" src="https://gist.githubusercontent.com/Micaeldevfs/22cba8fb7cd7fc9afd5fbbf0619cf8e4/raw/fa10cc3577f7c872f9b4d8091550724ca2cb78ff/infinityschool.svg" />, onde estudo linguagens como Python 🐍, Flask, HTML e CSS avançado, além de me aprofundar em JavaScript. <br />
+        <br />
+🎓 Além disso, estou cursando Análise e Desenvolvimento de Sistemas na Universidade <img align="center" alt="Unifor" src="https://gist.githubusercontent.com/Micaeldevfs/22cba8fb7cd7fc9afd5fbbf0619cf8e4/raw/fa10cc3577f7c872f9b4d8091550724ca2cb78ff/unifor.svg" width={25}  />, onde aprimoro meus conhecimentos teóricos e práticos para enfrentar os desafios da área de TI. <br />
+<br />
+💡 Sou apaixonado por resolver problemas e encontrar soluções inovadoras. Minha mentalidade proativa me impulsiona a sempre buscar novos desafios e aprender coisas novas. Acredito que o aprendizado contínuo é a chave para acompanhar o ritmo acelerado da tecnologia. <br />
+<br />
+💻 Seja bem-vindo(a) ao meu perfil do GitHub, onde compartilho projetos e experimentos que refletem minha jornada de aprendizado e evolução como desenvolvedor. Sinta-se à vontade para explorar e contribuir! Vamos aprender juntos! 🚀<br />
+</h3>
+<img src={imgprog} alt="" />
+<div>
+ <h3>Tecnologias que estou aprendendo atualmente no curso de programação da Infinity School</h3>
+
+  <img align="center" alt="python" src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" />
+  <img align="center" alt="flask" src="https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white" />
+  <img align="center" alt="html5" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+  <img align="center" alt="css" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+  <img align="center" alt="js" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+  <img align="center" alt="react" src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
+</div><br/>
+
+
+
+<h4> Tecnologias que pretendo aprender logo após terminar o curso:</h4>
+
+ <img align="center" alt="typescript" src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" />
+ <img align="center" alt="node.js" width="100px" src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" />
+
+    </>
+  )
+}
+
+export default App
